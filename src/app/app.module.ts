98dd8702a6414f8fe378cf9c2fd0c2fa40041799
LuisNataan <luisnataan556@gmail.com>
2022-10-05 18:04:30 -0300

    Develop: Updating app component with new imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterSupplierComponent } from './register-supplier/register-supplier.component';
+import { RegisterCustomerComponent } from './register-customer/register-customer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterSupplierComponent,
+    RegisterCustomerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
