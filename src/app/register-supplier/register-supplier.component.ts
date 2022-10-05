231fd613abd3b485b86600d6df095388c2df4050
LuisNataan <luisnataan556@gmail.com>
2022-10-05 18:03:13 -0300

    Develop: Creating register supplier component.

---
+++ src/app/register-supplier/register-supplier.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register-supplier.component.html',
+  styleUrls: ['./register-supplier.component.css']
+})
+export class RegisterSupplierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
