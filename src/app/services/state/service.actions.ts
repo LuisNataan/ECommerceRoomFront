6814e30f050cc3bf54d21562ad202ac53f76630c
LuisNataan <luisnataan556@gmail.com>
2022-10-06 15:57:54 -0300

    Develop: Creating service actions.

---
+++ src/app/services/state/service.actions.ts
@@ -0,0 +1,36 @@
+import { createAction, props } from "@ngrx/store";
+import { ResponseModel } from "src/app/interfaces/response/response.model";
+import { Customer } from "src/app/interfaces/customer.model";
+import { Supplier } from "src/app/interfaces/supplier.model";
+
+
+export const registerCustomer = createAction(
+  '[Customer] Create Customer',
+  props<{ customer: Customer }>()
+)
+
+export const registerCustomerSuccess = createAction(
+  '[ResponseModel] Register Success',
+  props<{ registerResponse: ResponseModel }>()
+);
+
+export const registerCustomerFailure = createAction(
+  '[ResponseModel] Register Failure',
+  props<{ error: ResponseModel }>()
+);
+
+export const registerSupplier = createAction(
+  '[Supplier] Create Supplier',
+  props<{ supplier: Supplier }>()
+)
+
+export const registerSupplierSuccess = createAction(
+  '[ResponseModel] Register Success',
+  props<{ registerResponse: ResponseModel }>()
+);
+
+export const registerSupplierFailure = createAction(
+  '[ResponseModel] Register Failure',
+  props<{ error: ResponseModel }>()
+);
+
