0b1614f4cc21a817f07e9031e9c08ca68f07aad7
LuisNataan <luisnataan556@gmail.com>
2022-10-06 15:59:07 -0300

    Develop: Updating app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { RegisterSupplierComponent } from './register-supplier/register-supplier.component';
-import { RegisterCustomerComponent } from './register-customer/register-customer.component';
+import { SupplierComponent } from './pages/supplier/supplier.component';
+import { CustomerComponent } from './pages/customer/customer.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    RegisterSupplierComponent,
-    RegisterCustomerComponent,
-    HeaderComponent
+    SupplierComponent,
+    CustomerComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
