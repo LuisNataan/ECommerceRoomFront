e89fb24c3a8b3d94bff49f11d6a6a59da25bce4d
LuisNataan <luisnataan556@gmail.com>
2022-10-06 15:02:09 -0300

    Develop: Inserting routes into app-routing-module .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
+import { SupplierComponent } from './pages/supplier/supplier.component';
+import { CustomerComponent } from './pages/customer/customer.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: 'home',
+  component: HomeComponent,
+  loadChildren: () => import('./pages/home/home.component').then(m => m.HomeComponent),
+  data: {
+    pageTitle: 'Home'
+  }
+},
+{
+  path: 'customer',
+  component: CustomerComponent,
+  loadChildren: () => import('./pages/customer/customer.component').then(m => m.CustomerComponent),
+  data: {
+    pageTitle: 'Customer'
+  }
+},
+{
+  path: 'supplier',
+  component: SupplierComponent,
+  loadChildren: () => import('./pages/supplier/supplier.component').then(m => m.SupplierComponent),
+  data: {
+    pageTitle: 'Supplier'
+  }
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
