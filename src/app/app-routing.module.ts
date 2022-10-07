d7f6a3a20a255149e9bcca0f7c1b633940a33993
LuisNataan <luisnataan556@gmail.com>
2022-10-07 09:55:19 -0300

    Develop: Refactoring Routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,30 +5,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-{
-  path: 'home',
-  component: HomeComponent,
-  loadChildren: () => import('./pages/home/home.component').then(m => m.HomeComponent),
-  data: {
-    pageTitle: 'Home'
-  }
-},
-{
-  path: 'customer',
-  component: CustomerComponent,
-  loadChildren: () => import('./pages/customer/customer.component').then(m => m.CustomerComponent),
-  data: {
-    pageTitle: 'Customer'
-  }
-},
-{
-  path: 'supplier',
-  component: SupplierComponent,
-  loadChildren: () => import('./pages/supplier/supplier.component').then(m => m.SupplierComponent),
-  data: {
-    pageTitle: 'Supplier'
-  }
-}
+{ path: 'home', component: HomeComponent },
+{ path: 'customer', component: CustomerComponent },
+{ path: 'supplier', component: SupplierComponent }
 ];
 
 @NgModule({
