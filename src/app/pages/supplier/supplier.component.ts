a1d8c000d622f20e268bd40732a81afbdee9d5b7
LuisNataan <luisnataan556@gmail.com>
2022-10-07 16:18:40 -0300

    Develop: Configuring Supplier component.

--- src/app/pages/supplier/supplier.component.ts
+++ src/app/pages/supplier/supplier.component.ts
@@ -1,16 +1,54 @@
+import { SupplierService } from './../../services/state/supplier/supplier.service';
+import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'app-register',
+  selector: 'app-supplier',
   templateUrl: './supplier.component.html',
   styleUrls: ['./supplier.component.css']
 })
 export class SupplierComponent implements OnInit {
-  title = 'supplier'
+title = 'supplier';
+  public supplierForm!: FormGroup
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private supplierService: SupplierService
+  ) { }
 
   ngOnInit(): void {
+    this.supplierForm = this.formBuilder.group({
+      name: [''],
+      corporateName: [''],
+      email: [''],
+      phoneNumber: [''],
+      einNumber: [''],
+      city: [''],
+      state: [''],
+      streetName: [''],
+      number: [''],
+      zipCode: ['']
+    });
+  }
+
+  createSupplier() {
+    let supplier: any = {
+      Name: this.supplierForm.controls['name'].value,
+      CorporateName: this.supplierForm.controls['corporateName'].value,
+      Email: this.supplierForm.controls['email'].value,
+      PhoneNumber: this.supplierForm.controls['phoneNumber'].value,
+      EinNumber: this.supplierForm.controls['einNumber'].value,
+      AddressViewModel: {
+        City: this.supplierForm.controls['city'].value,
+        State: this.supplierForm.controls['state'].value,
+        StreetName: this.supplierForm.controls['streetName'].value,
+        Number: this.supplierForm.controls['number'].value,
+        ZipCode: this.supplierForm.controls['zipCode'].value
+      }
+    }
+    this.supplierService.register(supplier);
+    alert('Supplier successfully registered.')
   }
 
 }
