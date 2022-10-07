6a61f69e028c3688ea421866dda14fff1e656458
LuisNataan <luisnataan556@gmail.com>
2022-10-07 16:21:17 -0300

    Develop: Configuring Customer component.

--- src/app/pages/customer/customer.component.ts
+++ src/app/pages/customer/customer.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { selectCustomerList } from 'src/app/services/state/actions.selector';
+import { CustomerService } from 'src/app/services/state/customer/customer.service';
+import * as ServiceActions from 'src/app/services/state/service.actions';
 
 @Component({
   selector: 'app-customer',
@@ -9,21 +13,46 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 export class CustomerComponent implements OnInit {
   title = 'customer';
 
-  public registerForm!: FormGroup
+  public customerForm!: FormGroup
+
+  customers$ = this.store.select(selectCustomerList);
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private store: Store,
+    private customerService: CustomerService
   ) { }
 
   ngOnInit(): void {
-    this.registerForm = this.formBuilder.group({
+    this.customerForm = this.formBuilder.group({
       name: [''],
       email: [''],
-      phoneNumber: ['']
+      phoneNumber: [''],
+      city: [''],
+      state: [''],
+      streetName: [''],
+      number: [''],
+      zipCode: ['']
+
     });
   }
 
-  customerForm(){
+  createCustomer() {
+    this.store.dispatch(ServiceActions.registerCustomer(
+      {
+        customer: {
+          name: this.customerForm.controls['name'].value,
+          email: this.customerForm.controls['email'].value,
+          phoneNumber: this.customerForm.controls['phoneNumber'].value,
+          addressViewModel: {
+            city: this.customerForm.controls['city'].value,
+            state: this.customerForm.controls['state'].value,
+            streetName: this.customerForm.controls['streetName'].value,
+            number: this.customerForm.controls['number'].value,
+            zipCode: this.customerForm.controls['zipCode'].value
+          }
+        }
+      }))
     alert('Customer successfully registered.')
   }
 }
